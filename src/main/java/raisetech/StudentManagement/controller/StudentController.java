ad7c5d644ca4d9ac2106fc4ec5f87597d6620af3
akimoto <sachinon19@gmail.com>
2024-10-21 16:27:52 +0900

    第14回演習課題 画面描画処理の追加

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,19 +1,19 @@
 package raisetech.StudentManagement.controller;
 
+//import ch.qos.logback.core.model.Model;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
-import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class StudentController {
 
     private StudentService service;
@@ -26,11 +26,12 @@ public class StudentController {
     }
 
     @GetMapping("/studentList")
-    public List<StudentDetail> getstudentlist() {
+    public String getStudentList(Model model) {
         List<Student> students = service.searchStudentList();
         List<StudentsCourses> studentsCourses = service.searchStudentCoursesList();
 
-        return converter.convertStudentDetails(students, studentsCourses);
+        model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+        return "studentList";
     }
 
 
