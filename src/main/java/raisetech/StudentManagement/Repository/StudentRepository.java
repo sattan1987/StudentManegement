70ce1b7aeef83ac2b96505f4182ca6d6d8b9803e
akimoto <sachinon19@gmail.com>
2024-11-29 21:01:31 +0900

    第17回演習課題 キャンセルボックスの作成とキャンセルされたものを表示させなくする機能の追加 不便に感じた部分の変更

--- src/main/java/raisetech/StudentManagement/Repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/Repository/StudentRepository.java
@@ -6,21 +6,17 @@ import raisetech.StudentManagement.data.StudentsCourses;
 
 import java.util.List;
 
-
 @Mapper
 public interface StudentRepository {
 
-
-    @Select("SELECT * FROM students")
+    @Select("SELECT * FROM students WHERE isDeleted = false")
     List<Student> search();
-
 
     @Select("SELECT * FROM students_courses")
     List<StudentsCourses> searchCourses();
 
-
-    @Insert("INSERT INTO students (name, furigana,nick_name, email_address, address, age, gender, remark) " +
-            "VALUES (#{name}, #{furigana}, #{nickName}, #{emailAddress}, #{address}, #{age}, #{gender}, #{remark})")
+    @Insert("INSERT INTO students (name, furigana, nick_name, email_address, address, age, gender, remark, isDeleted) " +
+            "VALUES (#{name}, #{furigana}, #{nickName}, #{emailAddress}, #{address}, #{age}, #{gender}, #{remark}, false)")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void saveStudent(Student student);
 
@@ -29,12 +25,13 @@ public interface StudentRepository {
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void saveStudentCourses(StudentsCourses studentsCourses);
 
-    @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nick_name = #{nickName}, email_address = #{emailAddress}, address = #{address}, age = #{age}, gender = #{gender}, remark = #{remark} WHERE id = #{id}")
+    @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nick_name = #{nickName}, email_address = #{emailAddress}, " +
+            "address = #{address}, age = #{age}, gender = #{gender}, remark = #{remark}, isDeleted = #{isDeleted} WHERE id = #{id}")
     void updateStudent(Student student);
 
-    @Update("UPDATE students_courses SET course = #{course}, enrollment_start_date = #{enrollmentStartDate}, enrollment_end_date = #{enrollmentEndDate} WHERE id = #{id} AND student_id = #{studentId}")
+    @Update("UPDATE students_courses SET course = #{course}, enrollment_start_date = #{enrollmentStartDate}, " +
+            "enrollment_end_date = #{enrollmentEndDate} WHERE id = #{id} AND student_id = #{studentId}")
     void updateStudentCourses(StudentsCourses studentsCourses);
-
 
     @Select("SELECT * FROM students WHERE id = #{id}")
     Student findById(int id);
@@ -45,4 +42,6 @@ public interface StudentRepository {
     @Select("SELECT * FROM students_courses WHERE id = #{id}")
     StudentsCourses findCourseById(int id);
 
+    @Update("UPDATE students SET isDeleted = true WHERE id = #{id}")
+    void softDeleteStudent(int id);
 }
