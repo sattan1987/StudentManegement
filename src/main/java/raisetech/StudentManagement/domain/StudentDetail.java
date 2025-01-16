ab42bfe01bbf445e1353ee7c1fd3dd6a0563e7fe
akimoto <sachinon19@gmail.com>
2025-01-16 11:37:06 +0900

    第25回演習課題 API仕様書の自動生成のための変更

--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,13 +11,17 @@ import raisetech.StudentManagement.data.StudentCourse;
 
 import java.util.List;
 
+@Schema(description = "受講生詳細")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentDetail {
+
+    @Schema(description = "受講生情報")
     @Valid
     private Student student;
+    @Schema(description = "受講生が登録しているコースの情報")
     @Valid
     private List<StudentCourse> studentCourseList;
 }
